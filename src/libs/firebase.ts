0935f931905db3ee099005ee0d45c83ae035a5a9
{cocoide} <{cocoide.com@gmail.com}>
2022-11-15 21:43:26 +0900

    ./libs/firebase.ts fetch eport to firebaseConfig

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -5,7 +5,7 @@ import 'firebase/firestore'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
-    const firebaseConfig = {
+    export const firebaseConfig = {
       apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
       authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
